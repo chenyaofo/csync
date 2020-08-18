c48ac147dc2ef84f21fa8e6491f2ba487bd33687
chenyaofo <chenyaofo@gmail.com>
2020-08-18 15:04:13 +0800

    fix dev suffix.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "pycodesync",
-    version = "0.1.1",
+    version = "0.1.2",
     description="Code sync command line application.",
     author="chenyaofo",
     author_email = "chenyaofo@gmail.com",
